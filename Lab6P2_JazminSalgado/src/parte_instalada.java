f342c443dfb3ac9ba7dda76e27394a1e5deb5bad
evasalgado <imviarsha@gmail.com>
2023-11-17 13:58:55 -0600

    clase de partes agregada

--- Lab6P2_JazminSalgado/src/parte_instalada.java
+++ Lab6P2_JazminSalgado/src/parte_instalada.java
@@ -8,5 +8,47 @@
  * @author evaja
  */
 public class parte_instalada {
+    private String marca;
+    private String clasificacion;
+    private double precio;
+
+    public parte_instalada() {
+    }
+
+    public parte_instalada(String marca, String clasificacion, double precio) {
+        this.marca = marca;
+        this.clasificacion = clasificacion;
+        this.precio = precio;
+    }
+    
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getClasificacion() {
+        return clasificacion;
+    }
+
+    public void setClasificacion(String clasificacion) {
+        this.clasificacion = clasificacion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return  "marca=" + marca + ", clasificacion=" + clasificacion + ", precio=" + precio + '}';
+    }
+    
     
 }
